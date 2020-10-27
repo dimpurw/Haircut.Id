83f8706d9b9029a2c949020745a2fb3952ef2457
DimasPratamaP <terminatorid2@gmail.com>
2020-10-27 23:39:00 +0700

    cuma nyoba ngeroutes ke dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/d', function () {
+    return view('dashboard');
+});
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
+
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
