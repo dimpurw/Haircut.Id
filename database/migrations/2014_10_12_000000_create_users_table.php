57902f05d7218e102ab1334320bde3d4ab02f26c
DimasPratamaP <terminatorid2@gmail.com>
2020-10-29 00:22:59 +0700

    foto nya

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,8 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->date('TTL');
-            $table->string('alamat');
             $table->string('foto');
+            $table->string('alamat');
             $table->rememberToken();
             $table->timestamps();
         });
