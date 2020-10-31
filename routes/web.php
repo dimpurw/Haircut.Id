7edb74fa422e47f8140f94ed15b87eebdccef72f
DimasPratamaP <terminatorid2@gmail.com>
2020-11-01 00:03:00 +0700

    buat routes nya..

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'PageController@home');
+Route::get('/registerbarbershop', 'PageController@registerbarbershop');
+Route::get('/registerpelanggan', 'PageController@registerpelanggan')->name('register');
+Route::post('/postregisterbarbershop', 'PageController@postregisterbarbershop');
+Route::post('/postregisterpelanggan', 'PageController@postregisterpelanggan');
+
+
+Route::get('login', 'AuthController@login')->name('login');
+Route::post('/postlogin', 'AuthController@postlogin');
+Route::get('/logout', 'AuthController@logout');
+
+Route::group(['middleware' => ['auth', 'CheckRole:pelanggan,barbershop,admin']], function () {
+    Route::get('/home', 'HomeController@index');
+    Route::get('/dashboards', 'DashboardController@dashboard');
 });
 
-Route::get('/d', function () {
-    return view('dashboard');
-});
+// Route::get('/d', function () {
+//     return view('page.dashboard');
+// });
 
-Route::get('/login', function () {
-    return view('auth.login');
-});
+// Route::get('/login', function () {
+//     return view('auth.login');
+// });
 
 
-Auth::routes();
+// Auth::routes();
 
-// verifikasi email
-Auth::routes(['verify' => true]);
+// // verifikasi email
+// Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
