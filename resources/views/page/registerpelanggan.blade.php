5ec58129080c7c04afe318be096657831e583deb
DimasPratamaP <terminatorid2@gmail.com>
2020-11-16 23:54:21 +0700

    udah bisa pop up

--- resources/views/page/registerpelanggan.blade.php
+++ resources/views/page/registerpelanggan.blade.php
@@ -5,21 +5,26 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                @if (session('sukses'))
+                <div class="alert alert-primary" role="alert">
+                    {{ session('sukses') }}<a href="/login" class="alert-link"> Silahkan Login</a>
+                </div>
+                @endif
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="/postregisterpelanggan" enctype="multipart/form-data">
+                    <form method="POST" action="/postregisterpelanggan" enctype="multipart/form-data" name="form" onsubmit="return validateForm()">
                         @csrf
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
+                            <label for="nama" class="col-md-4 col-form-label text-md-right">{{ __('Nama') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('nama') is-invalid @enderror" name="nama" value="{{ old('nama') }}" required autocomplete="nama" autofocus>
+                                <input id="nama" type="text" class="form-control @error('nama') is-invalid @enderror" name="nama" value="{{ old('nama') }}" autocomplete="nama" autofocus>
 
                                 @error('nama')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
+                                <div id="validationServer03Feedback" class="invalid-feedback">
+                                    {{$message}}
+                                </div>
                                 @enderror
                             </div>
                         </div>
@@ -28,7 +33,7 @@
                             <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" autocomplete="username" autofocus>
 
                                 @error('username')
                                 <span class="invalid-feedback" role="alert">
@@ -42,7 +47,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -56,7 +61,7 @@
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
                             </div>
                         </div>
 
@@ -64,7 +69,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Email') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus>
 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -78,7 +83,7 @@
                             <label for="TGL" class="col-md-4 col-form-label text-md-right">{{ __('Tanggal Lahir') }}</label>
 
                             <div class="col-md-6">
-                                <input id="TGL" type="date" class="form-control @error('TGL') is-invalid @enderror" name="TGL" value="{{ old('TGL') }}" required autocomplete="TGL" autofocus>
+                                <input id="TGL" type="date" class="form-control @error('TGL') is-invalid @enderror" name="TGL" value="{{ old('TGL') }}" autocomplete="TGL" autofocus>
 
                                 @error('TGL')
                                 <span class="invalid-feedback" role="alert">
@@ -92,7 +97,7 @@
                             <label for="alamat" class="col-md-4 col-form-label text-md-right">{{ __('Alamat') }}</label>
 
                             <div class="col-md-6">
-                                <input id="alamat" type="text" class="form-control @error('alamat') is-invalid @enderror" name="alamat" value="{{ old('alamat') }}" required autocomplete="alamat" autofocus>
+                                <input id="alamat" type="text" class="form-control @error('alamat') is-invalid @enderror" name="alamat" value="{{ old('alamat') }}" autocomplete="alamat" autofocus>
 
                                 @error('alamat')
                                 <span class="invalid-feedback" role="alert">
@@ -106,7 +111,7 @@
                             <label for="nomortelepon" class="col-md-4 col-form-label text-md-right">{{ __('Nomor Telepon') }}</label>
 
                             <div class="col-md-6">
-                                <input id="nomortelepon" type="number" class="form-control @error('nomortelepon') is-invalid @enderror" name="nomortelepon" value="{{ old('nomortelepon') }}" required autocomplete="nomortelepon" autofocus>
+                                <input id="nomortelepon" type="number" class="form-control @error('nomortelepon') is-invalid @enderror" name="nomortelepon" value="{{ old('nomortelepon') }}" autocomplete="nomortelepon" autofocus>
 
                                 @error('nomortelepon')
                                 <span class="invalid-feedback" role="alert">
@@ -137,4 +142,24 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    function validateForm() {
+        var a = document.forms["form"]["nama"].value;
+        var b = document.forms["form"]["username"].value;
+        var c = document.forms["form"]["password"].value;
+        var d = document.forms["form"]["password-confirm"].value;
+        var e = document.forms["form"]["email"].value;
+        var f = document.forms["form"]["TGL"].value;
+        var g = document.forms["form"]["alamat"].value;
+        var h = document.forms["form"]["nomortelepon"].value;
+        var i = document.forms["form"]["foto"].value;
+        if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "",
+            e == null || e == "", f == null || f == "", g == null || g == "", h == null || h == "",
+            i == null || i == "") {
+            alert("Data Tidak Boleh Kosong");
+            return false;
+        }
+    }
+</script>
 @endsection
