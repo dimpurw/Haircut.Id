43379cc8f1b6e676fc4a182a32573602fe60e02f
DimasPratamaP <terminatorid2@gmail.com>
2020-11-17 20:58:13 +0700

    view veerif

---
+++ resources/views/page/verifotp.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Verif Kode OTP') }}</div>
+
+                <div class="card-body">
+                    @if (session('success'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('success') }}
+                    </div>
+                    @endif
+
+                    <form method="POST" action="{{ route('activationtoken') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="token" class="col-md-4 col-form-label text-md-right">{{ __('Kode OTP') }}</label>
+
+                            <div class="col-md-6">
+                                <input type="text" class="form-control @error('email') is-invalid @enderror" name="active_token" id="active_token" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Send Password Reset Link') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
