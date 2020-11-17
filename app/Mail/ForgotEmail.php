3d81b33b93340bc1cb4a63d0ec836342d88c6767
DimasPratamaP <terminatorid2@gmail.com>
2020-11-17 20:53:40 +0700

    mail update

---
+++ app/Mail/ForgotEmail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\User;
+
+class ForgotEmail extends Mailable
+{
+    public $user;
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('Reset Password')->markdown('page.kodeotp')->with('user', $this->user);
+    }
+}
