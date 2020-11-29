3b22ebf6c23ee4114c03fd5e176632f32fe817a5
DimasPratamaP <terminatorid2@gmail.com>
2020-11-29 23:49:47 +0700

    up

---
+++ app/Paket.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Paket extends Model
+{
+    protected $table = 'paket';
+    protected $fillable = [
+        'barber_id', 'barbershop_id', 'layanan', 'harga',
+    ];
+
+    public function barber()
+    {
+        return $this->belongsTo('App\Barber');
+    }
+
+    public function barbershop()
+    {
+        return $this->belongsTo('App\Barbershop');
+    }
+}
