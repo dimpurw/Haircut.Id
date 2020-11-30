8c632f907a47651455be69c8005069fb2e572d41
DimasPratamaP <terminatorid2@gmail.com>
2020-11-30 23:36:12 +0700

    tambahan booking

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Booking;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -20,6 +23,35 @@ class HomeController extends Controller
     public function detail($id)
     {
         $barbershop = \App\Barbershop::find($id);
-        return view('page.detail', ['detail' => $barbershop]);
+        $data_barbershop = \App\Barbershop::all();
+        return view('page.detail', [
+            'barbershop' => $barbershop,
+            'data_barbershop' => $data_barbershop,
+        ]);
+    }
+
+    public function bookingshow($id)
+    {
+        $booking = \App\Barber::find($id);
+        return view('page.booking', ['booking' => $booking]);
+    }
+
+    public function checkout($id)
+    {
+        $user = \App\Pelanggan::find($id);
+        $booking = \App\Booking::find($id);
+        $paket = \App\Paket::all();
+        return view('page.checkout', [
+            'user' => $user,
+            'booking' => $booking,
+            'paket' => $paket,
+        ]);
+    }
+
+    public function order(Request $request, $id)
+    {
+        $booking = \App\Booking::find($id);
+        $booking->update($request->all());
+        return redirect(url('/home'))->with('success', 'data berhasil diubah');
     }
 }
