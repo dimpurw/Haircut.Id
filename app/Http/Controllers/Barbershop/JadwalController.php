ded3b919b10ca59a73dfba5cdbaf96e7df8599b7
DimasPratamaP <terminatorid2@gmail.com>
2020-12-01 23:11:33 +0700

    update lagiii

---
+++ app/Http/Controllers/Barbershop/JadwalController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Barbershop;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Booking;
+
+class JadwalController extends Controller
+{
+    public function index($id)
+    {
+        $data_barber = \App\Barbershop::find($id);
+        return view('barbershop.jadwal', ['data_barber' => $data_barber]);
+    }
+
+    public function create($id)
+    {
+        $barber = \App\Barbershop::find($id);
+        return view('barbershop.createjadwal', ['barber' => $barber]);
+    }
+
+    public function store(Request $request, $id)
+    {
+        $booking = new Booking();
+        $booking->barbershop_id = $request->barbershop_id;
+        $booking->barber_id = $request->barber_id;
+        $booking->tanggal = $request->tanggal;
+        $booking->jam = $request->jam;
+        $booking->save();
+        return redirect('/dashboardsbarbershop');
+    }
+}
