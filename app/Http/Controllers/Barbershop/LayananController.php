18d3cb63e1970161ba328149c53819409fd9fa8c
DimasPratamaP <terminatorid2@gmail.com>
2020-12-06 22:04:06 +0700

    validasi

--- app/Http/Controllers/Barbershop/LayananController.php
+++ app/Http/Controllers/Barbershop/LayananController.php
@@ -22,6 +22,15 @@ class LayananController extends Controller
 
     public function store(Request $request, $id)
     {
+        $request->validate([
+            'nama_barber' => 'required',
+            'email' => 'required',
+            'alamat' => 'required',
+            'nomortelepon' => 'required|max:13',
+            'keahlian' => 'required',
+            'foto' => 'required'
+        ]);
+
         $paket = new Paket();
         $paket->barbershop_id = $request->barbershop_id;
         $paket->barber_id = $request->barber_id;
