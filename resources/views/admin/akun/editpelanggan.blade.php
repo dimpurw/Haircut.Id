a570d37b9d2a1ca181c3b03874e7e44db1d02322
DimasPratamaP <terminatorid2@gmail.com>
2020-12-12 23:08:14 +0700

    up

--- resources/views/admin/akun/editpelanggan.blade.php
+++ resources/views/admin/akun/editpelanggan.blade.php
@@ -8,7 +8,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h4 class="card-title">Edit Data Pelanggan</h4>
-                        <form class="forms-sample" action="/akunpelanggan/{{$pelanggan->id}}" method="POST">
+                        <form class="forms-sample" action="/akunpelanggan/{{$pelanggan->id}}" method="POST" name="form" onsubmit="return validateForm()">
                             @method('patch')
                             @csrf
                             <div class="form-group">
@@ -17,7 +17,7 @@
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail3">Tanggal Lahir</label>
-                                <input type="date" name="TGL" class="form-control" id="exampleInputEmail3" value="{{$pelanggan->TGL}}">
+                                <input type="date" name="TGL" class="form-control" id="exampleInputEmail2" value="{{$pelanggan->TGL}}">
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail3">Alamat</label>
@@ -25,9 +25,9 @@
                             </div>
                             <div class="form-group">
                                 <label for="exampleInputEmail3">Nomor Telepon</label>
-                                <input type="number" name="nomortelepon" class="form-control" id="exampleInputEmail3" value="{{$pelanggan->nomortelepon}}">
+                                <input type="number" name="nomortelepon" class="form-control" id="exampleInputEmail4" value="{{$pelanggan->nomortelepon}}">
                             </div>
-                            <button type="submit" class="btn btn-gradient-primary mr-2">Submit</button>
+                            <button type="submit" class="btn btn-gradient-primary mr-2" onclick="sendMessage(); clearInput();">Submit</button>
                             <button class="btn btn-light">Cancel</button>
                         </form>
                     </div>
@@ -37,3 +37,15 @@
     </div>
 </div>
 @endsection
+<script type="text/javascript">
+    function validateForm() {
+        var a = document.forms["form"]["exampleInputName1"].value;
+        var b = document.forms["form"]["exampleInputName2"].value;
+        var c = document.forms["form"]["exampleInputName3"].value;
+        var d = document.forms["form"]["exampleInputName4"].value;
+        if (a == null || a == "", b == null || b == "", c == null || c == "", d == null || d == "") {
+            alert("username dan password tidak boleh kosong");
+            return false;
+        }
+    }
+</script>
