43a341691daf7f2fe5c2786ce15ad07782202509
DimasPratamaP <terminatorid2@gmail.com>
2020-12-15 15:57:53 +0700

    update model

--- app/Transaksi.php
+++ app/Transaksi.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaksi extends Model
 {
-    //
+    protected $table = 'transaksi';
+    protected $fillable = [
+        'invoice_id', 'pelanggan_id',  'barber_id', 'keterangan',
+    ];
 }
