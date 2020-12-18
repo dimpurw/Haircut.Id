a6fe355cb6b77da9ec55471bb4f343698ee763da
DimasPratamaP <terminatorid2@gmail.com>
2020-12-18 23:15:45 +0700

    up payment

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Booking;
+use App\Transaksi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DBuse;
+use Xendit\Xendit;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
@@ -14,6 +17,17 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
+
+    public function __construct()
+    {
+        $this->keyXendit = Xendit::setApiKey('xnd_development_zbk4MyoCcYlQtNjxNrLEZNoACVbXzllSLq8S7gcZok5FpTKEvD4WJ3EUCYlqTg');
+    }
+
+    public function GetSubCatAgainstMainCatEdit($id)
+    {
+        echo json_encode(DB::table('paket')->where('id', $id)->first());
+    }
+
     public function index()
     {
         $data_barbershop = \App\Barbershop::all();
@@ -56,8 +70,28 @@ class HomeController extends Controller
 
     public function order(Request $request, $id)
     {
+        $data = DB::table('paket')->where('id', $request->id)->first();
+
+        $email = $request->email;
+        $layanan = $data->layanan;
+        $harga = $data->harga;
+
+        $this->keyXendit;
+        $params = [
+            'external_id' => $id . '_haircut',
+            'payer_email' => $email,
+            'description' => $layanan,
+            'amount' => $harga
+        ];
+
+        $createInvoice = \Xendit\Invoice::create($params);
+        $transaksi = new Transaksi;
+        $transaksi->invoice_id = $createInvoice['id'];
+        $transaksi->save();
+        $x = \Xendit\Invoice::retrieve($createInvoice['id']);
+
         $booking = \App\Booking::find($id);
         $booking->update($request->all());
-        return redirect(url('/home'))->with('success', 'data berhasil diubah');
+        return redirect(url($x['invoice_url']))->with('success', 'data berhasil diubah');
     }
 }
